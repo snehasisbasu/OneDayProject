d3c8f21c42831769b77d75bcb17100d630a0602b
Snehasis Basu <snehasisbasu@Desktop.com>
2023-03-15 19:52:20 +0530

    Frontend Complete

--- OneDayProject/settings.py
+++ OneDayProject/settings.py
@@ -54,7 +54,9 @@ ROOT_URLCONF = 'OneDayProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            'Templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,6 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "Static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
